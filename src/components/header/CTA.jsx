46f732e61506f97b3bf55a132f467bd25cb07096
Pranudech <pranudech@gmail.com>
2023-01-04 15:57:56 +0700

    #Update link cv

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CTA = () => {
   return (
     <div className="cta">
-      <a className="btn" href={"assets/cv.pdf"} target="_blank" rel="noopener noreferrer">
+      <a className="btn" href="https://www.canva.com/design/DAE-Nosq5xw/Z5sujj5R5lcyKzS5GIJ6cA/view?utm_content=DAE-Nosq5xw&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" target="_blank" rel="noopener noreferrer">
         Download CV
       </a>
       <a className="btn btn-primary" href="#conract">Let's Talk</a>
