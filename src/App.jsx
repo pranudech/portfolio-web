aa11e805b5d87a6d11611d7ef4bfd74dc3f6214b
Pranudech <pranudech@gmail.com>
2023-01-09 13:45:36 +0700

    update nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
@@ -11,10 +11,25 @@ import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 
 const App = () => {
+  const [activeNav, setactiveNav] = useState("#");
+  window.addEventListener("scroll", () => {
+    if (window.scrollY === 0) {
+      setactiveNav("#");
+    } else if (window.scrollY >= 311 && window.scrollY <= 1355) {
+      setactiveNav("#about");
+    } else if (window.scrollY >= 1356 && window.scrollY <= 2172) {
+      setactiveNav("#experience");
+    } else if (window.scrollY >= 2173 && window.scrollY <= 2959) {
+      setactiveNav("#service");
+    } else if (window.scrollY > 2960) {
+      setactiveNav("#contact");
+    }
+  });
+
   return (
     <>
       <Header />
-      <Nav />
+      <Nav activeNav={activeNav} setactiveNav={setactiveNav} />
       <About />
       <Experience />
       <Service />
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai';
 import { BiBook, BiMessageDetail } from 'react-icons/bi';
 import { RiServiceLine } from 'react-icons/ri';
@@ -6,16 +6,14 @@ import { MdWorkOutline } from 'react-icons/md';
 import './nav.css';
 
 
-const Nav = () => {
-  const [activeNav, setactiveNav] = useState('#');
-
+const Nav = (props) => {
   return (
     <nav>
-      <a href='#' onClick={()=> setactiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
-      <a href='#about' onClick={()=> setactiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
-      <a href='#experience' onClick={()=> setactiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
-      <a href='#service' onClick={()=> setactiveNav('#service')} className={activeNav === '#service' ? 'active' : ''}><MdWorkOutline/></a>
-      <a href='#contact' onClick={()=> setactiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageDetail/></a>
+      <a href='#' onClick={()=> props.setactiveNav('#')} className={props.activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href='#about' onClick={()=> props.setactiveNav('#about')} className={props.activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+      <a href='#experience' onClick={()=> props.setactiveNav('#experience')} className={props.activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
+      <a href='#service' onClick={()=> props.setactiveNav('#service')} className={props.activeNav === '#service' ? 'active' : ''}><MdWorkOutline/></a>
+      <a href='#contact' onClick={()=> props.setactiveNav('#contact')} className={props.activeNav === '#contact' ? 'active' : ''}><BiMessageDetail/></a>
     </nav>
   )
 }
