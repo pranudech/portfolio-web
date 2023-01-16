908810c4c65a0bdd46c28ff8a3277cf3c45ddf00
Pranudech <pranudech@gmail.com>
2023-01-16 11:27:10 +0700

    update cv

--- src/components/header/HerderSocials.jsx
+++ src/components/header/HerderSocials.jsx
@@ -4,9 +4,9 @@ import { BsLinkedin, BsGithub, BsYoutube } from 'react-icons/bs'
 const HerderSocials = () => {
   return (
     <div className="header_socials">
-      <a href="https://github.com/pranudech" target="_blank"><BsLinkedin/></a>
+      <a href="https://www.linkedin.com/in/pranudech-reungwijit-4b9448243/" target="_blank"><BsLinkedin/></a>
       <a href="https://github.com/pranudech" target="_blank"><BsGithub/></a>
-      <a href="https://github.com/pranudech" target="_blank"><BsYoutube/></a>
+      {/* <a href="https://github.com/pranudech" target="_blank"><BsYoutube/></a> */}
     </div>
   );
 };
